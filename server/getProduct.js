baf81325843548a32301dd8fe26e16d3b4a5ddd4
Cathy Richardson <cathyanne08@gmail.com>
2021-07-03 16:00:51 -0600

    completed through number 6

--- server/getProduct.js
+++ server/getProduct.js
@@ -1,14 +1,24 @@
 const products = require('../products.json')
 
-let getProduct = (req, res) => {
-    console.log(req.params.id)
-    let idNum = parseInt(req.params.id, 10)
+let getProduct = (request, response) => {
+    let idNum = parseInt(request.params.id, 10)
     let filtered = products.filter(e => idNum === e.id)
     if (filtered.length > 0) {
-        res.status(200).json(filtered[0]);
+        response.status(200).json(filtered[0]);
     } else {
-        res.status(500).json('Item not in list');
+        response.status(500).json('Item not in list');
     }
 }
 
 module.exports = getProduct;
+
+
+//using the .find()
+// const getProduct = (req, res) => {
+//     // find returns the item if it finds it, or undefined if not
+//     const item = products.find((val) => val.id === parseInt(req.params.id))
+//     if (!item) {
+//       return res.status(500).send('Item not in list')
+//     }
+//     res.status(200).send(item)
+//   }
--- server/getProducts.js
+++ server/getProducts.js
@@ -1,7 +1,13 @@
 const products = require('../products.json')
 
-let getProducts = (req, res) => {
-    res.status(200).json(products);
+let getProducts = (request, response) => {
+    if (request.query.price) {
+        let productPrice = parseFloat(request.query.price)
+        let responseProducts = products.filter(e => e.price >= productPrice);
+        response.status(200).json(responseProducts);
+    } else {
+        response.status(200).json(products);
+    }
 }
 
 module.exports = getProducts;
